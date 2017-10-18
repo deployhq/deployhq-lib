a933a668390e96aba542e408edcca95769db0b8a
Dan Wentworth <dan@atechmedia.com>
2017-10-18 16:11:35 +0100

    configurable websocket location

--- lib/deploy/configuration.rb
+++ lib/deploy/configuration.rb
@@ -1,6 +1,11 @@
 module Deploy
   class Configuration
     attr_accessor :account, :username, :api_key, :project
+    attr_writer :websocket_hostname
+
+    def websocket_hostname
+      @websocket_hostname || 'wss://websocket.deployhq.com'
+    end
 
     def self.from_file(path)
       file_contents = File.read(path)
@@ -11,6 +16,7 @@ module Deploy
         config.username = parsed_contents['username']
         config.api_key = parsed_contents['api_key']
         config.project = parsed_contents['project']
+        config.websocket_hostname = parsed_contents['websocket_hostname']
       end
     end
   end
